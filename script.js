ddcd21cdab98c1784a5f4da3a843c0f3fbd102ca
thaliawoods <thaliadelano@gmail.com>
2024-06-04 20:58:31 +0200

    Dictionnaire

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ let bubbleSort = (inputArr) => {
 };
 
 console.log(bubbleSort([2, 54, 7, 38, 22, 13, 19, 45, 31, 77, 98, 5, 12, 28, 81, 26, 72, 4, 20, 42]))
+
+// Écrire une fonction qui prend deux listes en arguments et retourne un dictionnaire ayant pour clés la première liste et pour valeurs la deuxième, en associant les clés et valeurs par index de leurs listes respectives.
+
+function createDict(keys, values) {
+    let dict = {};
+    for (let i = 0; i < keys.length; i++) {
+        dict[keys[i]] = values[i];
+    }
+    return dict;
+}
+
+console.log(createDict(['a', 'b', 'c'], [1, 2, 3]));
